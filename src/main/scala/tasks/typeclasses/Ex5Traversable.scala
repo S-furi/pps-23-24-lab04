77981dd841677a46077357262ae5ef546fdda4b9
S-furi <stefano.furi@studio.unibo.it>
2024-03-25 18:30:14 +0100

    feat(ex5): complete task 5

--- src/main/scala/tasks/typeclasses/Ex5Traversable.scala
+++ src/main/scala/tasks/typeclasses/Ex5Traversable.scala
@@ -1,5 +1,7 @@
 package u04lab
 import u03.Sequences.* 
+import u03.Optionals.* 
+import u03.Optionals.Optional.*
 import Sequence.*
 
 /*  Exercise 5: 
@@ -23,4 +25,24 @@ object Ex5Traversable:
     case Cons(h, t) => log(h); logAll(t)
     case _ => ()
 
-  
+  trait Traversable[T[_]]:
+    extension [A](t: T[A]) def apply(c: A => Unit): Unit
+
+  given Traversable[Sequence] with
+    extension [A](t: Sequence[A]) def apply(c: A => Unit): Unit = t match
+      case Cons(head, tail) => c(head); tail.apply(c)
+      case _ =>
+
+  given Traversable[Optional] with
+    extension [A](t: Optional[A]) def apply(c: A => Unit): Unit = t match
+      case Just(a) => c(a)
+      case _ =>
+
+@main def main() =
+    import u04lab.Ex5Traversable.{*, given}
+    val opt = Just(10)
+    val seq = Cons(10, Cons(20, Cons(30, Nil())))
+
+    opt.apply(log)
+    seq.apply(log)
+
